9279012217a9f2020f3f394669679427a7d3f4a8
Maximilien Klima <maxeklima@gmail.com>
2022-11-08 14:13:11 +0100

    login functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import LoginScreen from './components/LoginScreen';
 import RegisterScreen from './components/RegisterScreen';
 import Chat from './components/Chat';
-
+import { useState } from 'react';
 
 function App() {
+  const [isLogged,setisLogged] = useState(false);
   
-  
-  return (
-    <div className="App">
-     <LoginScreen />
-    </div>
-  );
-}
+  if(isLogged){
+    return(
+      <Chat />
+    )
+  }
+  else{
+    return(
+      <LoginScreen setisLogged={setisLogged} isLogged={isLogged} />
+    )
+  }
 
+}
 export default App;
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
 
-
-function LoginScreen() {
+function LoginScreen({setisLogged, isLogged}) {
     const [email,setemail] = useState("");
     const [password,setpassword] = useState("");
     
@@ -14,20 +15,31 @@ function LoginScreen() {
         setpassword(e.target.value);
     }
 
-    
+    const login = () => {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("User logged in: " + user)
+            setisLogged(true);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error)
+        });
+    }
+
     return (
         <div className='MainContainer'>
             <div className='LoginContainer'>
-                <form>
                 <p>Email</p>
-                <input type="text"></input>
+                <input type="text" onChange={handleEmail}></input>
                 <p>Password</p>
-                <input type="text"></input>
+                <input type="text" onChange={handlePassword}></input>
                 <br></br>
-                <button className='LoginButton'>Login</button>
+                <button className='LoginButton' onClick={login}>Login</button>
                 <br></br>
                 <h1><a href='X'>Dont have an account?</a></h1>
-                </form>
             </div>
         </div>
     )
