9b7757a92ab0de195a8886d7dada0e09024967e4
Maximilien Klima <maxeklima@gmail.com>
2023-01-30 12:44:09 +0100

    Cleared Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 import { getStorage } from "firebase/storage";
 
+//paste here your firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyCTecrRoU_4oRAHsTHvWk-2V2aaiMsRuC4",
-    authDomain: "chat-app-17edb.firebaseapp.com",
-    projectId: "chat-app-17edb",
-    storageBucket: "chat-app-17edb.appspot.com",
-    messagingSenderId: "845243678229",
-    appId: "1:845243678229:web:c20fc186d23449d00fd81c",
-    measurementId: "G-MT4QZ80QSX"
-  };
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  
-  export { auth, db };
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
